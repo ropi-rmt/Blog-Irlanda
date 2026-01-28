15818d6d6398b84ff1f72e110fee5befb28e8452
ropi-rmt <ropi.rmt@gmail.com>
2026-01-28 07:09:59 -0300

    creando un js para contacto

---
+++ js/contacto.js
@@ -0,0 +1,33 @@
+//Formulario Submit
+
+const formulario = document.getElementById("miFormulario")
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+
+    console.log(nombre);
+    console.log(email);
+    console.log(mensaje);
+
+    localStorage.setItem("nombreUsuario", nombre);
+    localStorage.setItem("emailUsuario", email);
+
+    console.log("Datos guardados en localStorage");
+    formulario.reset()
+});
+
+const respuesta = document.getElementById("respuesta");
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    respuesta.textContent = "Gracias por tu mensaje 😊";
+});
+
+
