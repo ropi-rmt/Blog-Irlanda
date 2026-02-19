237acfc03f0d3eaa49795bfec7f6ca117881b036
ropi-rmt <ropi.rmt@gmail.com>
2026-02-19 11:29:30 -0300

    mejorando comments js

--- js/comments.js
+++ js/comments.js
@@ -2,20 +2,11 @@
 const formulario = document.getElementById("formComentario");
 const listaComentarios = document.getElementById("listaComentarios");
 
-function mostrarComentario(nombre, comentario) {
-    const nuevoComentario = document.createElement("p");
-    nuevoComentario.textContent = `${nombre}: ${comentario}`;
-    listaComentarios.appendChild(nuevoComentario);
-}
+const comentariosGuardados = JSON.parse(localStorage.getItem("comentarios")) || [];
 
-document.addEventListener("DOMContentLoaded", function () {
-    const comentariosGuardados = JSON.parse(localStorage.getItem("comentarios")) || [];
-
-    comentariosGuardados.forEach(com => {
-        mostrarComentario(com.nombre, com.comentario);
-    });
+comentariosGuardados.forEach(com => {
+    mostrarComentario(com.nombre, com.comentario);
 });
-
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -23,18 +14,14 @@ formulario.addEventListener("submit", function (event) {
     const nombre = document.getElementById("nombre").value;
     const comentario = document.getElementById("comentario").value;
 
-
     mostrarComentario(nombre, comentario);
 
 
     const comentariosGuardados = JSON.parse(localStorage.getItem("comentarios")) || [];
 
-
     comentariosGuardados.push({ nombre, comentario });
 
-
     localStorage.setItem("comentarios", JSON.stringify(comentariosGuardados));
-
 
     formulario.reset();
 });
