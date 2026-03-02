77d9c9643e4acc4035943e34936d0b8cdf7344e4
ropi-rmt <ropi.rmt@gmail.com>
2026-03-02 07:29:04 -0300

    ruta de fetch

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function agregarCiudades(ciudades) {
 
 async function cargarCiudades() {
     try {
-        const response = await fetch("./js/ciudades.json"); 
+        const response = await fetch("../js/ciudades.json"); 
         const data = await response.json();
         agregarCiudades(data);
     } catch (error) {
